a460e2f0108588ffe33df8d0044aebea5509fe65
Katie Park <38256522+katie-park@users.noreply.github.com>
2020-11-24 21:26:50 -0500

    initial upload

---
+++ format_prereqs.py
@@ -0,0 +1,12 @@
+from courses import AtoZ
+import re
+
+def janky_format_prereq(prereq_str: str) -> str:
+	janky_str = prereq_str
+	useless_phrases = ["Undergraduate Semester level", "Minimum Grade of C", "Minimum Grade of D"]
+	for phrase in useless_phrases:
+		janky_str = janky_str.replace(phrase,"")
+	return " ".join(janky_str.split()) # trims external and *internal* whitespace (pretty inefficient lol)
+
+prereq_str = input("Enter prerequisite text to be formatted:\n")
+print("\n" + janky_format_prereq(prereq_str))
